085391a97c5cbbbc8677d092cc91215fa7cd0926
rezafauzan <rezafauzan593@gmail.com>
2026-02-19 15:16:54 +0700

    Feat: add table relation; Fix: wrong data type

--- base.sql
+++ base.sql
@@ -11,6 +11,7 @@ CREATE TABLE buku (
         category_id INT,
         borrower_id INT,
         lender_id INT,
+        bookshelf_id INT,
         borrowed_at TIMESTAMP,
         back_at TIMESTAMP,
         created_at TIMESTAMP DEFAULT NOW(),
@@ -27,7 +28,7 @@ CREATE TABLE kategori (
 
 DROP TABLE IF EXISTS rak_buku;
 CREATE TABLE rak_buku (
-        id VARCHAR(255) PRIMARY KEY,
+        id SERIAL PRIMARY KEY,
         shelf_name VARCHAR(255),
         created_at TIMESTAMP DEFAULT NOW(),
         updated_at TIMESTAMP DEFAULT NOW()
@@ -49,3 +50,8 @@ CREATE TABLE peminjam(
         created_at TIMESTAMP DEFAULT NOW(),
         updated_at TIMESTAMP DEFAULT NOW()
 );
+
+ALTER TABLE "buku" ADD CONSTRAINT fk_kategori FOREIGN KEY (category_id) REFERENCES kategori(id)
+ALTER TABLE "buku" ADD CONSTRAINT fk_peminjam FOREIGN KEY (borrower_id) REFERENCES peminjam(id)
+ALTER TABLE "buku" ADD CONSTRAINT fk_petugas FOREIGN KEY (lender_id) REFERENCES petugas(id)
+ALTER TABLE "buku" ADD CONSTRAINT fk_rak_buku FOREIGN KEY (bookshelf_id) REFERENCES rak_buku(id)
