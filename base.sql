7daac130eda9b3c109e11446aa16e1a78c983977
rezafauzan <rezafauzan593@gmail.com>
2026-02-19 14:59:40 +0700

    Fix: extra "," in last table column; add default now to created at

--- base.sql
+++ base.sql
@@ -1,45 +1,41 @@
-CREATE DATABASE library
+CREATE DATABASE library;
 
 CREATE TABLE buku (
         id SERIAL PRIMARY KEY,
         title VARCHAR(255),
         author VARCHAR(255),
         publisher VARCHAR(255),
-        total_pages int(4),
+        total_pages INT,
         category_id INT,
         borrower_id INT,
         lender_id INT,
         borrowed_at TIMESTAMP,
         back_at TIMESTAMP,
-        created_at TIMESTAMP,
-        updated_at TIMESTAMP,
-)
-
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP
+);
 CREATE TABLE kategori (
         id SERIAL PRIMARY KEY,
         category_name VARCHAR(255),
-        created_at TIMESTAMP,
-        updated_at TIMESTAMP,
-)
-
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP
+);
 CREATE TABLE rak_buku (
         id VARCHAR(255) PRIMARY KEY,
         shelf_name VARCHAR(255),
-        created_at TIMESTAMP,
-        updated_at TIMESTAMP,
-)
-
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP
+);
 CREATE TABLE petugas(
-        id PRIMARY KEY VARCHAR(255),
+        id SERIAL PRIMARY KEY,
         name VARCHAR(255),
-        created_at TIMESTAMP,
-        updated_at TIMESTAMP,
-)
-
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP
+);
 CREATE TABLE peminjam(
-        id PRIMARY KEY VARCHAR(255),
+        id SERIAL PRIMARY KEY,
         name VARCHAR(255),
-        book_id FK VARCHAR(255),
-        created_at TIMESTAMP,
-        updated_at TIMESTAMP,
-)
+        book_id INT,
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP
+);
